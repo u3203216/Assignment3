e643c15360fd9d6350d8a59ec38c5096a96db9e2
u3203216 <113828366+u3203216@users.noreply.github.com>
2022-10-23 12:46:40 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+﻿
+const links = document.querySelectorAll('ul li');
+const pages = document.querySelectorAll('.page');
+const allBtns = document.querySelectorAll('.close-btn');
+
+for (let i = 0; i < allBtns.length; i++) {
+    allBtns[i].addEventListener('click', function () {
+        allBtns[i].parentElement.style.display = "none";
+        document.querySelector('header').style.filter = "blur(0px)";
+    });
+}
+
+$('.gallery li').hide();
+
+$('.gallery .trigger').on('click', function () {
+
+    $('.gallery li').each(function (index) {
+        $(this).delay(1000 * index).fadeIn(1000);
+    });
+
+});
+
+let selectedPageBtn = "";
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        for (let i = 0; i < pages.length; i++) {
+            pages[i].style.display = "none";
+        }
+        pages[i].style.display = "block";
+        selectedPageBtn = i;
+        document.querySelector('header').style.filter = "blur(2px)";
+    });
+}
+
